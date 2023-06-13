2adfed0a11b0ae36dcaacfd916fb70ad23916505
ignxal <ignacio.logiudice@gmail.com>
2023-06-13 12:33:41 -0300

    feat(sprint1): user - add and get all

--- app/controllers/UsuarioController.php
+++ app/controllers/UsuarioController.php
@@ -4,73 +4,57 @@ require_once './interfaces/IApiUsable.php';
 
 class UsuarioController extends Usuario implements IApiUsable
 {
-    public function CargarUno($request, $response, $args)
-    {
-        $parametros = $request->getParsedBody();
+  public function CargarUno($request, $response, $args)
+  {
+    $parametros = $request->getParsedBody();
 
-        $usuario = $parametros['usuario'];
-        $clave = $parametros['clave'];
+    $usuario = $parametros['usuario'];
+    $clave = $parametros['clave'];
+    $tipo = $parametros['tipo'];
 
-        // Creamos el usuario
-        $usr = new Usuario();
-        $usr->usuario = $usuario;
-        $usr->clave = $clave;
-        $usr->crearUsuario();
+    if (isset($parametros['usuario']) && isset($parametros['clave']) && isset($parametros['tipo'])) {
+      $usr = new Usuario();
+      $usr->usuario = $usuario;
+      $usr->clave = $clave;
+      $usr->tipo = $tipo;
+      $usr->crearUsuario();
 
-        $payload = json_encode(array("mensaje" => "Usuario creado con exito"));
-
-        $response->getBody()->write($payload);
-        return $response
-          ->withHeader('Content-Type', 'application/json');
+      $payload = json_encode(array("mensaje" => "Usuario creado con exito"));
+    } else {
+      $payload = json_encode(array("mensaje" => "Datos incompletos"));
     }
+    // Creamos el usuario
 
-    public function TraerUno($request, $response, $args)
-    {
-        // Buscamos usuario por nombre
-        $usr = $args['usuario'];
-        $usuario = Usuario::obtenerUsuario($usr);
-        $payload = json_encode($usuario);
+    $response->getBody()->write($payload);
+    return $response
+      ->withHeader('Content-Type', 'application/json');
+  }
 
-        $response->getBody()->write($payload);
-        return $response
-          ->withHeader('Content-Type', 'application/json');
-    }
+  public function TraerTodos($request, $response, $args)
+  {
+    $lista = Usuario::obtenerTodos();
+    $payload = json_encode(array("listaUsuario" => $lista));
 
-    public function TraerTodos($request, $response, $args)
-    {
-        $lista = Usuario::obtenerTodos();
-        $payload = json_encode(array("listaUsuario" => $lista));
+    $response->getBody()->write($payload);
+    return $response
+      ->withHeader('Content-Type', 'application/json');
+  }
 
-        $response->getBody()->write($payload);
-        return $response
-          ->withHeader('Content-Type', 'application/json');
-    }
-    
-    public function ModificarUno($request, $response, $args)
-    {
-        $parametros = $request->getParsedBody();
+  public function ModificarUno($request, $response, $args)
+  {
+    $payload = json_encode(array("mensaje" => "Función Próximamente!"));
 
-        $nombre = $parametros['nombre'];
-        Usuario::modificarUsuario($nombre);
+    $response->getBody()->write($payload);
+    return $response
+      ->withHeader('Content-Type', 'application/json');
+  }
 
-        $payload = json_encode(array("mensaje" => "Usuario modificado con exito"));
+  public function BorrarUno($request, $response, $args)
+  {
+    $payload = json_encode(array("mensaje" => "Función Próximamente!"));
 
-        $response->getBody()->write($payload);
-        return $response
-          ->withHeader('Content-Type', 'application/json');
-    }
-
-    public function BorrarUno($request, $response, $args)
-    {
-        $parametros = $request->getParsedBody();
-
-        $usuarioId = $parametros['usuarioId'];
-        Usuario::borrarUsuario($usuarioId);
-
-        $payload = json_encode(array("mensaje" => "Usuario borrado con exito"));
-
-        $response->getBody()->write($payload);
-        return $response
-          ->withHeader('Content-Type', 'application/json');
-    }
+    $response->getBody()->write($payload);
+    return $response
+      ->withHeader('Content-Type', 'application/json');
+  }
 }
--- app/interfaces/IApiUsable.php
+++ app/interfaces/IApiUsable.php
@@ -1,7 +1,6 @@
 <?php
 interface IApiUsable
 {
-	public function TraerUno($request, $response, $args);
 	public function TraerTodos($request, $response, $args);
 	public function CargarUno($request, $response, $args);
 	public function BorrarUno($request, $response, $args);
